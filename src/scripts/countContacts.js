fbbf7ef0fd856bf7aaf64f0028edb9598cb99a19
prihodko_bohdan <prihodkob15@gmail.com>
2024-09-29 12:31:52 +0300

    hw1

--- src/scripts/countContacts.js
+++ src/scripts/countContacts.js
@@ -1,3 +1,12 @@
-export const countContacts = async () => {};
+import { readContacts } from '../utils/readContacts.js';
 
-console.log(await countContacts());
+export const countContacts = async () => {
+    try {
+        const contacts = await readContacts();
+        return contacts.length;
+    } catch (error) {
+        console.error('Error counting contacts:', error);
+    }
+};
+
+countContacts().then((count) => console.log(`Number of contacts: ${count}`));
--- src/utils/writeContacts.js
+++ src/utils/writeContacts.js
@@ -1,3 +1,12 @@
+import { promises as fs } from 'fs';
 import { PATH_DB } from '../constants/contacts.js';
 
-export const writeContacts = async (updatedContacts) => {};
+export const writeContacts = async (updatedContacts) => {
+    try {
+    const formattedData = JSON.stringify(updatedContacts, null, 2);
+    await fs.writeFile(PATH_DB, formattedData, 'utf8');
+    console.log('Contacts have been written successfully.');
+  } catch (error) {
+    console.error('Error writing contacts:', error);
+  }
+};
